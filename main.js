28decb45b0804488d16730ff0c2b14cf141b67f5
sandramuhire55 <sandramuhire5@gmail.com>
2022-02-14 15:06:29 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// let emplolee = {
+//     name:"sandra",
+//     adress:"Mahama",
+//     phonenumber: "0780470009",
+//     email: "sandra@gamil.com",
+//     age: 20,
+//     maired: false
+//  }
+//  console.log(emplolee)
+//  functiom////
+
+
+function sum(){
+    let a=20;
+    let b=34;
+    let sum=a+b;
+
+    return sum;
+}
+// function call
+console.log(sum())
+
+// function displayName(){
+//     let name="sandra";
+//     return name;
+// }
+// //function call
+// console.log(displayName());
+// // jhtttt
+//  function areaofscquale(radius){
+//      let area=radius*radius+3.14;
+//      return area;
+//  }
+// //  function call///
+// //console.log(sum())
+// console.log(displayName());
+// console.log(areaofscquale(8))
